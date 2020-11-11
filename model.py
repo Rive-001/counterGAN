a0c741432363308906775a592d236b600de63120
Ubuntu <ubuntu@ip-172-31-12-86.us-east-2.compute.internal>
2020-11-11 00:15:53 +0000

    Denoise added

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision.datasets as datasets
 import torch.utils.data as data
+from vgg import VGG
 
 
 class Generator(nn.Module):
@@ -69,16 +70,20 @@ class counterGAN():
 
         self.netG = Generator(self.nz).to(self.device)
         self.netD = Discriminator().to(self.device)
+        self.netTarget = VGG('VGG16').to(self.device)
 
         self.optimizerG = optim.Adam(self.netG.parameters(), lr=2e-4, betas=(self.beta1,0.999))
         self.optimizerD = optim.Adam(self.netD.parameters(), lr=2e-4, betas=(self.beta1,0.999))
         self.criterion = nn.BCELoss()
+        self.criterionTarget = nn.CrossEntropyLoss()
 
 
     def train(self,init_epoch, num_epochs,dataloader):
 
         G_losses = []
         D_losses = []
+        T_losses = []
+        Losses = []
 
         for epoch in range(init_epoch,num_epochs):
             for idx,D in enumerate(dataloader['train']):
@@ -87,6 +92,7 @@ class counterGAN():
                 self.netD.zero_grad()
 
                 image = D[0].to(self.device)
+                target_labels = D[1].to(self.device)
                 batch_size = image.size()[0]
                 label = torch.full((batch_size,),self.real_label,dtype=torch.float,device=self.device)
 
@@ -102,7 +108,7 @@ class counterGAN():
                 generated = self.netG(noise)
                 label.fill_(self.fake_label)
 
-                out_generated = self.netD(generated.detach()).view(-1)
+                out_generated = self.netD(generated.detach()+image).view(-1)
                 loss_d_generated = self.criterion(out_generated,label)
                 loss_d_generated.backward()
 
@@ -113,23 +119,33 @@ class counterGAN():
 
                 #Train Generator
                 self.netG.zero_grad()
+                self.netTarget.zero_grad()
 
                 label.fill_(self.real_label)
 
-                out_generated_2 = self.netD(generated).view(-1)
+                out_generated_2 = self.netD(generated+image).view(-1)
                 loss_g = self.criterion(out_generated_2,label)
-                loss_g.backward()
+                loss_g.backward(retain_graph=True)
+
+                out_classifier = self.netTarget(generated+image)
+                loss_c = self.criterionTarget(out_classifier,target_labels)
+                loss_c.backward()
 
                 D_G_z2 = out_generated_2.mean().item()
+
+                loss = loss_c+loss_g
+
                 self.optimizerG.step()
 
                 if idx%50==0:
-                    print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                    print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tOverall loss: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
                   % (epoch, num_epochs, idx, len(dataloader['train']),
-                     loss_d.item(), loss_g.item(), D_x, D_G_z1, D_G_z2))
+                     loss_d.item(), loss_g.item(), loss.item(), D_x, D_G_z1, D_G_z2))
                 
                 G_losses.append(loss_g.item())
                 D_losses.append(loss_d.item())
+                T_losses.append(loss_c.item())
+                Losses.append(loss.item())
 
         return D_losses,G_losses
 

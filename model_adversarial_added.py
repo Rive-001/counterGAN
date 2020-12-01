de80fd950be8f564a7224a656b01d09ab4c04d0e
Rishi <rishiv@andrew.cmu.edu>
2020-12-01 16:13:11 -0500

    Comments

--- model_adversarial_added.py
+++ model_adversarial_added.py
@@ -78,6 +78,8 @@ class counterGAN():
         self.netD = Discriminator().to(self.device)
         self.netTarget = VGG('VGG16').to(self.device)
         self.netTarget.load_state_dict(torch.load('BestClassifierModel.pth',map_location=self.device))
+
+        # fixed_noise -> stores fixed generator seed for inference
         self.fixed_noise = torch.randn(64, self.nz, 1, 1, device=self.device)
 
         self.netG.apply(self.weights_init)
@@ -87,6 +89,8 @@ class counterGAN():
         self.optimizerD = optim.Adam(self.netD.parameters(), lr=2e-4, betas=(self.beta1,0.999))
         self.criterion = nn.BCELoss()
         self.criterionTarget = nn.CrossEntropyLoss()
+
+        # criterionPerturbation -> norm of the generated noise
         self.criterionPerturbation = nn.MSELoss()
 
     def weights_init(self,m):
@@ -103,7 +107,6 @@ class counterGAN():
             'optimizerD_state_dict':self.optimizerD.state_dict()
         },path)
         
-        # print('Saved state dicts')
 
     def load_state_dicts(self,path):
         
@@ -142,12 +145,14 @@ class counterGAN():
                 image_adv = D_adv[0].to(self.device)
                 target_labels_adv = D_adv[1].to(self.device)
                 batch_size_adv = image_adv.size()[0]
+                # label_adv -> target label to use for adversarial images while training discriminator
                 label_adv = torch.full((batch_size_adv,),self.fake_label,dtype=torch.float,device=self.device)
                 
                 #real images definition
                 image = D[0][:batch_size_adv,...].to(self.device)
                 target_labels = D[1].to(self.device)
                 batch_size = image.size()[0]
+                # label -> target label to use for real images while training discriminator
                 label = torch.full((batch_size,),self.real_label,dtype=torch.float,device=self.device)
                 
 
@@ -155,12 +160,11 @@ class counterGAN():
                 loss_d_real = self.criterion(out_real,label)
                 # loss_d_real.backward()
 
+                # D_x -> output of the discriminator for real images. Between (0,1)
                 D_x = out_real.mean().item()
 
                 #Train Discriminator on generated images
                 noise = torch.randn(batch_size,self.nz,1,1,device=self.device)
-                # if D[0].shape[0]!=D_adv[0].shape[0]:
-                #     import ipdb; ipdb.set_trace()
                 generated = self.netG(noise)+image_adv
                 label_adv = torch.full((batch_size,),self.fake_label,dtype=torch.float,device=self.device)
 
@@ -168,8 +172,10 @@ class counterGAN():
                 loss_d_generated = self.criterion(out_generated,label_adv.detach())
                 # loss_d_generated.backward()
 
+                # D_G_z1 -> output of the discriminator for generated images. Between (0,1)
                 D_G_z1 = out_generated.mean().item()
 
+                # loss_d -> total loss of discriminator
                 loss_d = loss_d_generated+loss_d_real
 
                 # self.optimizerD.step()
@@ -183,15 +189,19 @@ class counterGAN():
                 out_generated_2 = self.netD(generated.clone()).view(-1)
                 loss_g = self.criterion(out_generated_2,label_adv)
                 # loss_g.backward(retain_graph=True)
-
+                
+                # Calculate loss on classifier
                 out_classifier = self.netTarget(generated.clone())
                 loss_c = self.criterionTarget(out_classifier,target_labels_adv)
                 # loss_c.backward()
 
+                # D_G_z2 -> output of the discriminator for generated images. Between (0,1)
                 D_G_z2 = out_generated_2.mean().item()
 
+                # Calculate norm of noise generated
                 loss_p = self.criterionPerturbation(image,generated)
 
+                # loss -> final loss
                 loss = loss_d+(loss_c*loss_c)+loss_g-loss_p
                 loss.backward()
                 self.optimizerD.step()
@@ -243,6 +253,12 @@ class counterGAN():
         plt.savefig('./images/Image_{}_{}.png'.format(epoch,img_type))
 
         return
+
+    def inference(self, images, fixed_noise=self.fixed_noise):
+
+        generated = (self.netG(self.fixed_noise)+images).detach().cpu().tolist()
+
+        return generated
 
 
 

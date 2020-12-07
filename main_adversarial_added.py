4fed7ee2f78b731d38f775e3a4fa5b902e8afc1c
Tarang Shah <tarang27@gmail.com>
2020-12-07 02:08:42 -0500

    Adding updates and linear experiment

--- main_adversarial_added.py
+++ main_adversarial_added.py
@@ -1,4 +1,5 @@
 import torch
+
 torch.manual_seed(0)
 import torch.nn as nn
 import torch.optim as optim
@@ -10,63 +11,95 @@ from model_adversarial_added import *
 import matplotlib.pyplot as plt
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
-torch.autograd.set_detect_anomaly(True)
+import os
+
+USE_WANDB = True
+if USE_WANDB:
+    import wandb
+
+    os.environ["WANDB_API_KEY"] = open("wandbkey").read()
+    wandb.init(project="IDL_PROJECT_COUNTERGAN")
+    params = wandb.config
+    wandb.save("main_adversarial_added.py")
+    wandb.save("model_adversarial_added.py")
+else:
+    params = {}
+
+params["round_name"] = "round0"
+params["batch_size"] = 784  # * 6  # 256 * 3
+params["lr"] = 1e-3
+params["momentum"] = 0.9
+params["epochs"] = 200
+
+# torch.autograd.set_detect_anomaly(True)
 num_workers = 4
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # device = torch.device('cpu')
 print(device)
-    
-    
+
+
 ##Load Datasets
 trans = {}
-trans['train'] = transforms.Compose([
-    # transforms.RandomCrop(32, padding=4),
-    # transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-trans['test'] = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+trans["train"] = transforms.Compose(
+    [
+        # transforms.RandomCrop(32, padding=4),
+        # transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ]
+)
+trans["test"] = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ]
+)
 
 Datasets = {}
-Datasets['train'] = datasets.CIFAR10(
-    root='./',train=True,transform=trans['train'],download=True
+Datasets["train"] = datasets.CIFAR10(
+    root="./", train=True, transform=trans["train"], download=True
 )
-Datasets['test'] = datasets.CIFAR10(
-    root='./',train=False,transform=trans['test'],download=True
+Datasets["test"] = datasets.CIFAR10(
+    root="./", train=False, transform=trans["test"], download=True
 )
 
-BATCH_SIZE = 784
-dataloaders = {x:data.DataLoader(Datasets[x], shuffle=True, batch_size=BATCH_SIZE, num_workers=num_workers) for x in ['train','test']}
+BATCH_SIZE = params["batch_size"]
+dataloaders = {
+    x: data.DataLoader(
+        Datasets[x], shuffle=True, batch_size=BATCH_SIZE, num_workers=num_workers
+    )
+    for x in ["train", "test"]
+}
 
-#Load Adverserial datasets
+# Load Adverserial datasets
 
 traindir = "./baseline_adv2/train"
 validdir = "./baseline_adv2/dev"
 
 data = {
-    'train':
-    datasets.ImageFolder(root=traindir, transform=trans['train']),
-    'test':
-    datasets.ImageFolder(root=validdir, transform=trans['test'])
+    "train": datasets.ImageFolder(root=traindir, transform=trans["train"]),
+    "test": datasets.ImageFolder(root=validdir, transform=trans["test"]),
 }
 
 # Dataloader iterators, make sure to shuffle
 dataloaders_adv = {
-    'train': DataLoader(data['train'], batch_size=BATCH_SIZE, shuffle=True,num_workers=num_workers),
-    'test': DataLoader(data['test'], batch_size=BATCH_SIZE, shuffle=True,num_workers=num_workers)
+    "train": DataLoader(
+        data["train"], batch_size=BATCH_SIZE, shuffle=True, num_workers=num_workers
+    ),
+    "test": DataLoader(
+        data["test"], batch_size=BATCH_SIZE, shuffle=True, num_workers=num_workers
+    ),
 }
-    
 
 
-
-
-
-NUM_EPOCHS = 200
-cgan = counterGAN(device, linear=True)
-D_losses, G_losses, T_losses, img_list_adv, img_list_real = cgan.train(0,NUM_EPOCHS,dataloaders,dataloaders_adv)
+NUM_EPOCHS = params["epochs"]
+if USE_WANDB:
+    cgan = counterGAN(device, lr=params["lr"], linear=True, wandb=wandb)
+else:
+    cgan = counterGAN(device, lr=params["lr"], linear=True)
+D_losses, G_losses, T_losses, img_list_adv, img_list_real = cgan.train(
+    0, NUM_EPOCHS, dataloaders, dataloaders_adv
+)
 
 # Search learning rate space
 # lrs = [10**i for i in np.random.uniform(-5,-2, 20)]
